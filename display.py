7af88e557479eac497754c4fc80f616ca950caea
voldo <filipecavoldo@outlook.com>
2026-09-08 18:13:07 -0300

    no more oop

--- display.py
+++ display.py
@@ -10,79 +10,95 @@ DISPLAY_HEIGHT = 32
 BLACK = 0
 WHITE = 255
 
-class Display():
+display = None
+buffer = None
+window = None
+image_data = None
 
-    def __init__(self):
-        # used to keep track which bits are on or OFF
-        self.display = [False] * (DISPLAY_WIDTH * DISPLAY_HEIGHT)
+def init_pyglet():
+    """initializes globals and returns (buffer, window)
+    for further manipulation
+    """
+    global display, buffer, window, image_data
+    # used to keep track which bits are on or OFF
+    display = [False] * (DISPLAY_WIDTH * DISPLAY_HEIGHT)
 
-        # a touple list of x, y values
-        # which represent fliped-on bits on the memory buffer
-        self.buffer = []
+    # a touple list of x, y values
+    # which represent fliped-on bits on the memory buffer
+    buffer = []
 
-        self.window = pyglet.window.Window()
-        display_bytes = (ctypes.c_ubyte * (DISPLAY_WIDTH * DISPLAY_HEIGHT))()
-        self.image_data = pyglet.image.ImageData(
-            DISPLAY_WIDTH,
-            DISPLAY_HEIGHT, 
-            "L",
-            display_bytes,
-            DISPLAY_WIDTH)
+    window = pyglet.window.Window(
+        width=DISPLAY_WIDTH * SCALE, height=DISPLAY_HEIGHT * SCALE)
 
-        self.window.width = DISPLAY_WIDTH * SCALE
-        self.window.height = DISPLAY_HEIGHT * SCALE
+    image_data = pyglet.image.ImageData(
+        DISPLAY_WIDTH,
+        DISPLAY_HEIGHT, 
+        "L",
+        (ctypes.c_ubyte * (DISPLAY_WIDTH * DISPLAY_HEIGHT))(),
+        DISPLAY_WIDTH)
 
-        self.i = 0
-        self.j = 0
-        pyglet.clock.schedule_interval(test.debug_fill_display, 0.001, self)
+    attach_handlers(window)
 
-        # consider calling this on another routine called directly by the CPU
-        def on_draw():
-            #self.debug_display()
-            self.update_pixels()
-            self.buffer.clear()
-            self.image_data.get_texture()
-            sprite = pyglet.sprite.Sprite(self.image_data)
-            sprite.scale = SCALE
-            sprite.draw()
+    pyglet.clock.schedule_interval(
+        debug_fill_display, 1/240, buffer)
 
+    return (buffer, window)
 
-        def on_mouse_press(x, y, button, mods):
-            """Debug only: puts on the buffer clicked location"""
-            print(f"mouse pressed on: ({x}, {y})")
-            x = int(x // SCALE)
-            y = int(y // SCALE)
-            self.buffer.append((x, y))
+def attach_handlers(window):
+    window.on_draw = on_draw
+    window.on_mouse_press = on_mouse_press
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
 
-        self.window.on_draw = on_draw
-        self.window.on_mouse_press = on_mouse_press
+def on_draw():
+    global display, buffer, window, image_data
+    update_pixels(image_data)
+    buffer.clear()
+    image_data.get_texture()
+    sprite = pyglet.sprite.Sprite(image_data)
+    sprite.scale = SCALE
+    sprite.draw()
 
-    def update_pixels(self):
-        byte_list = self.image_data.get_bytes()
+def on_mouse_press(x, y, button, mods):
+    """Debug only: puts on the buffer clicked location"""
+    global buffer
+    print(f"mouse pressed on: ({x}, {y})")
+    x = int(x // SCALE)
+    y = int(y // SCALE)
+    buffer.append((x, y))
 
-        for x, y in self.buffer:
-            index =  x + y * DISPLAY_WIDTH
-            if byte_list[index]:
-                byte_list[index] = BLACK
-            else:
-                byte_list[index] = WHITE
+def on_key_press(symbol, modifiers):
+    pass
 
-        self.image_data.set_bytes("L", DISPLAY_WIDTH, byte_list)
+def on_key_release(symbol, modifiers):
+    pass
 
-    def debug_display(self, dt):
-        """Fills the screen gradually from bottom to top"""
-        if (self.i >= DISPLAY_WIDTH):
-            self.i = 0
-            self.j += 1
-        if (self.j >= DISPLAY_HEIGHT):
-            self.j = 0
-        self.buffer.append((self.i, self.j))
-        self.i += 1
+def update_pixels(image_data):
+    byte_list = image_data.get_bytes()
 
-    def run(self):
-        pyglet.app.run()
+    for x, y in buffer:
+        index =  x + y * DISPLAY_WIDTH
+        #TODO remember to set one of the registers flag later
+        if byte_list[index]:
+            byte_list[index] = BLACK
+        else:
+            byte_list[index] = WHITE
+
+    image_data.set_bytes("L", DISPLAY_WIDTH, byte_list)
+
+i = 0
+j = 0
+def debug_fill_display(dt, buffer):
+    global i, j
+    """Fills the screen gradually from bottom to top"""
+    if (i >= DISPLAY_WIDTH):
+        i = 0
+        j += 1
+    if (j >= DISPLAY_HEIGHT):
+        j = 0
+    buffer.append((i, j))
+    i += 1
 
 
 if __name__ == "__main__":
-    window = Display()
-    window.run()
+    print("wrong module")
